531b8c708d69bf52b374a4bdaa703da604454844
Oniel <51058739+0niel@users.noreply.github.com>
2022-01-03 23:12:49 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Softglass',
       home: SafeArea(child: SoftglassWebView()),
     );
